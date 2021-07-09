264a408396fd656dc1f5d5c11e005f6fa1c031a4
chylex <contact@chylex.com>
2021-07-09 18:14:20 +0200

    Fix crash when installed on a server

--- src/main/java/chylex/bettercontrols/BetterControlsMod.java
+++ src/main/java/chylex/bettercontrols/BetterControlsMod.java
@@ -1,6 +1,10 @@
 package chylex.bettercontrols;
 import chylex.bettercontrols.config.BetterControlsConfig;
 import chylex.bettercontrols.gui.BetterControlsScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -10,10 +14,20 @@ import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("bettercontrols")
 public final class BetterControlsMod{
-	public static final BetterControlsConfig config = BetterControlsConfig.load(FMLPaths.CONFIGDIR.get().resolve("BetterControls.json"));
+	public static final BetterControlsConfig config = DistExecutor.safeCallWhenOn(Dist.CLIENT, () -> ClientLoader::loadConfig);
 	
 	public BetterControlsMod(){
-		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (mc, parentScreen) -> new BetterControlsScreen(parentScreen));
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> ClientLoader::createScreen);
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+	}
+	
+	private static final class ClientLoader {
+		public static BetterControlsConfig loadConfig() {
+			return BetterControlsConfig.load(FMLPaths.CONFIGDIR.get().resolve("BetterControls.json"));
+		}
+		
+		public static BetterControlsScreen createScreen(final Minecraft mc, final Screen parentScreen) {
+			return new BetterControlsScreen(parentScreen);
+		}
 	}
 }
